fff790db024d569b4bd3c7f7c35953661299d210
wjdgus1122 <qodqodantk@gmail.com>
2023-05-10 22:36:45 +0900

    array edit

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ let deleteBtn = document.querySelector(".deleteBtn");
 let noticeWrap = document.querySelector(".noticeWrap");
 let popup = document.querySelector(".noticePopupWrap");
 let popupClose = document.querySelector(".popupClose");
+let popupBtn = document.querySelector(".editBtn");
+let conArray = [];
 
-plusBtn.addEventListener("click", () => {
-  popup.style.display = "flex";
-});
+plusBtn.addEventListener("click", () => (popup.style.display = "flex"));
 
 popupClose.addEventListener("click", () => {
   document.getElementById("textInput").value = null;
@@ -14,3 +14,18 @@ popupClose.addEventListener("click", () => {
   document.getElementById("textArea").value = null;
   popup.style.display = "none";
 });
+
+popupBtn.addEventListener("click", function (e) {
+  conArray.push({
+    id: conArray.length,
+    title: document.getElementById("textInput").value,
+    date: document.getElementById("dateInput").value,
+    text: document.getElementById("textArea").value,
+  });
+});
+
+const arrayMap = () => {
+  conArray.map(() => {
+    let a = document.createElement;
+  });
+};
